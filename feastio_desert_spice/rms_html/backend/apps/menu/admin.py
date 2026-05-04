5aabf4733e39acf822a9f540d642a737187af9b3
sanishstha8 <xthasanish44@gmail.com>
2026-05-04 15:40:00 +0545

    modifying admin.py of menu

--- feastio_desert_spice/rms_html/backend/apps/menu/admin.py
+++ feastio_desert_spice/rms_html/backend/apps/menu/admin.py
@@ -1,15 +1,3 @@
-from django.contrib import admin
-from .models import Category, MenuItem
-
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'description']
-
-
-@admin.register(MenuItem)
-class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ['name', 'category', 'price', 'available']
-    list_filter = ['category', 'available']
-    search_fields = ['name']
-    list_editable = ['available', 'price']
