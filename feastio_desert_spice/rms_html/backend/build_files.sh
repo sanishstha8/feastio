6adb357c3d6d5235672d02583918afa2eecf1114
sanishstha8 <xthasanish44@gmail.com>
2026-07-01 16:20:15 +0545

    Fix pip install for externally-managed environment

--- feastio_desert_spice/rms_html/backend/build_files.sh
+++ feastio_desert_spice/rms_html/backend/build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing dependencies..."
-pip install -r requirements.txt
+pip install --break-system-packages -r requirements.txt
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
